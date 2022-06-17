1fca435eb9585f45b072fa9311e796f9d274d413
qinyouzeng <qinyouzeng@masastack.com>
2022-06-17 17:23:05 +0800

    1

---
+++ src/Services/Masa.Tsc.Service/Application/Logs/Queries/LogFieldQuery.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Service.Admin.Application;
+
+public record LogFieldQuery : Query<IEnumerable<Nest.MappingResponse>>
+{
+    public override IEnumerable<Nest.MappingResponse> Result { get; set; }
+}
+
