a40b52783bdd09ed762176f9a1e6c28198902a51
qinyouzeng <qinyouzeng@masastack.com>
2022-07-08 14:48:42 +0800

    feat: team detail

--- src/Web/Masa.Tsc.Admin/Shared/TscComponentBase.cs
+++ src/Web/Masa.Tsc.Admin/Shared/TscComponentBase.cs
@@ -1,12 +1,17 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+using Masa.Blazor;
+
 namespace Masa.Tsc.Admin.Rcl.Shared;
 
 public class TscComponentBase: ComponentBase
 {
     [Inject]
     public ILogger<TscComponentBase> Logger { get; set; }
+
+    [Inject]
+    public IPopupService PopupService { get; set; }
 
     protected override void OnAfterRender(bool firstRender)
     {
