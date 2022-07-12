57c5aa5a9d7af5976107fc7b49e021e62fd6ff2a
qinyouzeng <qinyouzeng@masastack.com>
2022-07-12 20:28:08 +0800

    feat: add log component

---
+++ src/Web/Masa.Tsc.Admin/Pages/Components/Incrument/Pannel/Log/TscLog.razor.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Admin.Rcl.Pages.Components;
+
+public partial class TscLog
+{
+    private int _lastedDuration = 1;
+
+    private List<KeyValuePair<int, string>> _dicDurations = default!;
+
+    private List<object> _data = default!;
+
+    private int _totalPage =1;
+}
