ea98592296b22328402588daa0d91ca5c787e0ed
qinyouzeng <qinyouzeng@masastack.com>
2022-07-14 16:15:15 +0800

    feat: elastic query update

---
+++ src/Contracts/Masa.Tsc.Contracts.Order/Logs/MappingResponse.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Contracts.Admin;
+
+public class MappingResponse
+{
+    public string Name { get; set; }
+
+    public string DataType { get; set; }
+
+    public bool? IsKeyword { get; set; }
+
+    /// <summary>
+    /// keyword 支持最长查询
+    /// </summary>
+    public int? MaxLenth { get; set; }
+}
