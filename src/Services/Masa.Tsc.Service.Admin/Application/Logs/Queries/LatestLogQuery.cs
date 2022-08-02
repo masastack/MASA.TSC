86e9409aec5fd6b919eb883e8c3bd279bcf74e6b
qinyouzeng <qinyouzeng@masastack.com>
2022-08-02 10:02:38 +0800

    feat: update command

---
+++ src/Services/Masa.Tsc.Service.Admin/Application/Logs/Queries/LatestLogQuery.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Service.Admin.Application.Logs;
+
+public record LatestLogQuery : Query<object>
+{
+    public string Query { get; set; }
+
+    public DateTime Start { get; set; }
+
+    public DateTime End { get; set; }
+
+    public bool IsDesc { get; set; } = true;
+
+    public override object Result { get; set; }
+}
---
+++ src/Services/Masa.Tsc.Service.Admin/Application/Logs/Queries/LogsQuery.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Service.Admin.Application.Logs;
+
+public record LogsQuery(string Query, DateTime Start, DateTime End, int Page = 1, int Size = 10, string Sort = "desc", string Duration = "15m") : Query<PaginationDto<object>>
+{
+    public override PaginationDto<object> Result { get; set; }
+}
