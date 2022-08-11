32189846ac54d872c11c249edff566127b9932c4
qinyouzeng <qinyouzeng@masastack.com>
2022-08-11 10:48:11 +0800

    chore: restore project componet

---
+++ src/Web/Masa.Tsc.Web.Admin/Pages/Components/Team/Project.razor.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Web.Admin.Rcl.Pages.Components;
+
+public partial class Project
+{
+    [Parameter]
+    public TeamDto Team { get; set; } = default!;
+
+    [Parameter]
+    public ProjectDto Value { get; set; } = default!;
+
+    [Parameter]
+    public string AppId { get; set; } = default!;
+}
