7c24f7ea89999d755afa331230c83c836ee4ea77
wzh425 <wuzihao425@outlook.com>
2022-11-08 11:41:34 +0800

    feature : datav

---
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Pages/DataV/Modules/LinkTrackingTopologys/LinkTrackingTopologyViewModel.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Web.Admin.Rcl.Pages.DataV.Modules.LinkTrackingTopologys;
+
+public class LinkTrackingTopologyViewModel
+{
+    public List<LinkTrackingTopologyNodeViewModel> Nodes { get; set; } = new();
+
+    public List<LinkTrackingTopologyEdgeViewModel> Edges { get; set; } = new();
+}
