d6aeadc086127148a66dfdfca270363f57a1a6c0
capdiem <capdiem@live.com>
2022-12-05 18:14:36 +0800

    🚧 feat: wip

---
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Panel/Trace/TraceResponseTree.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Web.Admin.Rcl.Components;
+
+public class TraceResponseTree : TraceResponseDto
+{
+    public TraceResponseTree(TraceResponseDto dto, int level)
+    {
+        Timestamp = dto.Timestamp;
+        EndTimestamp = dto.EndTimestamp;
+        TraceId = dto.TraceId;
+        SpanId = dto.SpanId;
+        ParentSpanId = dto.ParentSpanId;
+        Kind = dto.Kind;
+        TraceStatus = dto.TraceStatus;
+        Name = dto.Name;
+        Attributes = dto.Attributes;
+        Resource = dto.Resource;
+
+        Level = level;
+    }
+
+    public TraceResponseTree(TraceResponseDto dto, int level, List<TraceResponseTree> children) : this(dto, level)
+    {
+        Children = children;
+    }
+
+    public List<TraceResponseTree>? Children { get; set; }
+
+    public int Level { get; set; }
+
+    public List<TraceResponseTimeline> Timelines { get; set; } = new();
+}
+
+public record TraceResponseTimeline(bool Render, double Percent, double marginLeft = 0);
--- src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Panel/Trace/TscTraceDetail.razor.cs
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Panel/Trace/TscTraceDetail.razor.cs
@@ -6,167 +6,142 @@ namespace Masa.Tsc.Web.Admin.Rcl.Components;
 public partial class TscTraceDetail
 {
     [Parameter]
-    public string TraceId { get { return _traceId; } set { _traceId = value; _tabIndex = "attr"; } }
+    public string TraceId { get; set; }
 
-    private StringNumber _tabIndex = "attr";
-    private IEnumerable<TraceResponseDto> _items = new List<TraceResponseDto>();
     private bool _isLoading = true;
-    private string _traceId = default!;
-    private TraceDetailModel _selectItem;
-    private TraceOverviewModel _overView = new();
 
-    private Func<object, long> _timeUsFunc = obj =>
-    {
-        var dic = (Dictionary<string, object>)obj;
-        dic = (Dictionary<string, object>)(dic.ContainsKey("transaction") ? dic["transaction"] : dic["span"]);
-        return Convert.ToInt64(((Dictionary<string, object>)dic["duration"])["us"]);
+    private string _lastTraceId;
+    private List<TraceResponseTree> treeData = new();
+    private StringNumber tabValue;
 
-    };
-    private Func<object, DateTime> _timeFunc = obj =>
-    {
-        var dic = (Dictionary<string, object>)obj;
-        return DateTime.Parse(dic["@timestamp"].ToString()!);
-    };
-    private Func<object, string> _keyFunc { get; set; } = item =>
-    {
-        var dic = (Dictionary<string, object>)item;
-        return (dic.ContainsKey("transaction") ? GetDictionaryValue(dic, "transaction.id") : GetDictionaryValue(dic, "span.id")).ToString()!;
-    };
-    private Func<object, string> _parentFunc { get; set; } = item => GetDictionaryValue((Dictionary<string, object>)item, "parent.id")?.ToString() ?? string.Empty;
-
-    private List<DataTableHeader<Dictionary<string, object>>> _headers = new()
-    {
-        new("Service", item => ((Dictionary<string, object>)item["service"])["name"])
-        {
-            Align = DataTableHeaderAlign.Start,
-            Sortable = false
-        },
-        new("Endpoint", item => ((Dictionary<string, object>)item["transaction"])["name"])
-        {
-            Align = DataTableHeaderAlign.Start,
-            Sortable = false
-        },
-        new("Duration (ms)", item => ((Dictionary<string, object>)((Dictionary<string, object>)item["transaction"])["duration"])["us"])
-        {
-            Align = DataTableHeaderAlign.Start,
-            Sortable = false
-        },
-        new("Timestamp", item => item["@timestamp"])
-        {
-            Align = DataTableHeaderAlign.Start,
-            Sortable = false
-        },
-        new("EndTime", item => ((Dictionary<string, object>)item["transaction"])["name"])
-        {
-            Align = DataTableHeaderAlign.Start,
-            Sortable = false
-        }
-    };
+    private List<string>? actives;
+    private TraceResponseTree? _activeTreeItem;
 
     protected override async Task OnParametersSetAsync()
     {
-        _isLoading = true;
-        var data = await ApiCaller.TraceService.GetAsync(TraceId);
-        if (data == null)
-            return;
-
-        _items = data;
-        SetDeeep();
-        SetOverview();
-        await OnChangeRecordAsync(_items.FirstOrDefault()!);
         await base.OnParametersSetAsync();
+
+        if (_lastTraceId != TraceId)
+        {
+            _lastTraceId = TraceId;
+
+            await QueryTraceDetailAndToTree(TraceId);
+        }
+    }
+
+    private async Task QueryTraceDetailAndToTree(string traceId)
+    {
+        _isLoading = true;
+        var data = await ApiCaller.TraceService.GetAsync(traceId);
+        treeData = ToTree(data, null);
+        if (treeData.Any())
+        {
+            _activeTreeItem = treeData.First();
+            actives = new List<string>() { _activeTreeItem.SpanId };
+        }
+
         _isLoading = false;
     }
 
-    private async Task OnChangeRecordAsync(TraceResponseDto item)
+    private void OnActiveUpdate(List<TraceResponseTree> items)
     {
-        _selectItem = new TraceDetailModel(item);
-        StateHasChanged();
-        await Task.CompletedTask;
-    }
-
-    private static string CreateColor()
-    {
-        int length = 6;
-        var bytes = new int[length];
-        var index = 0;
-        do
+        if (items.Count == 0)
         {
-            bytes[index] = Random.Shared.Next(0, 16);
-            index++;
-        } while (length - index > 0);
-        return $"#{string.Join("", bytes.Select(b => b.ToString("X")))}";
-    }
-
-    private void SetDeeep()
-    {
-        if (_items == null || !_items.Any())
             return;
-        _overView.SpansDeeps.Clear();
-        _overView.SpanChildren.Clear();
+        }
 
-        var list = new List<TraceResponseDto>();
-        foreach (var item in _items)
+        _activeTreeItem = items.First();
+    }
+
+    private List<TraceResponseTree> ToTree(IEnumerable<TraceResponseDto> items, TraceResponseTree? parent, long parentLeft = 0)
+    {
+        int parentLevel = parent?.Level ?? 0;
+        string? parentSpanId = parent?.SpanId;
+
+        var currentLevel = parentLevel + 1;
+        var found = items.Where(item => item.ParentSpanId == parentSpanId);
+        var nodes = found.Select(item => new TraceResponseTree(item, currentLevel)).ToList();
+
+        foreach (var node in nodes)
         {
-            string id = item.SpanId, parentId = item.ParentSpanId ?? String.Empty;
-            long us = (long)Math.Floor((item.EndTimestamp - item.Timestamp).TotalMilliseconds);
-            DateTime time = item.Timestamp;
+            var items2 = items.Except(found).ToList();
+            node.Children ??= new();
 
-            if (_overView.SpansDeeps.ContainsKey(id))
-                continue;
-            list.Add(item);
-            int deep = 0;
-            bool isTransaction = string.IsNullOrEmpty(item.ParentSpanId) || !_items.Any(t => t.SpanId == item.ParentSpanId);
-            string name = item.Resource["service.name"].ToString()!;
-
-            var add = new TraceTableLineModel
+            long internalParentLeft = parentLeft;
+            if (parent is not null)
             {
-                Id = id,
-                ParentId = parentId,
-                Time = time,
-                Deep = deep,
-                IsTransaction = isTransaction,
-                ServiceName = name,
-                TimeUs = us
-            };
+                internalParentLeft += (long)Math.Ceiling((node.Timestamp - parent.Timestamp).TotalMilliseconds);
+            }
 
-            if (string.IsNullOrEmpty(parentId))
-                _overView.SpansDeeps.Add(id, add);
+            var children = ToTree(items2, node, internalParentLeft).OrderBy(u => u.Timestamp).ToList();
+
+            if (children.Any())
+            {
+                node.Children.AddRange(children);
+
+                if (internalParentLeft > 0)
+                {
+                    Console.Out.WriteLine("internalParentLeft = {0}", internalParentLeft);
+                    var marginLeft = internalParentLeft / (double)parent.Duration;
+
+                    var duration = (long)Math.Floor((node.EndTimestamp - node.Timestamp).TotalMilliseconds);
+                    var durationPercent = (duration / (double)parent.Duration);
+                    node.Timelines.Add(new TraceResponseTimeline(true, durationPercent, marginLeft));
+                }
+
+                var lastTimestamp = node.Timestamp;
+                foreach (var child in children)
+                {
+                    var duration = (long)Math.Floor((child.Timestamp - lastTimestamp).TotalMilliseconds);
+                    if (duration < 0)
+                    {
+                        continue;
+                    }
+
+                    node.Timelines.Add(new TraceResponseTimeline(true, duration / (double)node.Duration));
+
+                    var childDuration = child.Duration;
+                    node.Timelines.Add(new TraceResponseTimeline(false, childDuration / (double)node.Duration));
+
+                    lastTimestamp = child.EndTimestamp;
+                }
+
+                if (lastTimestamp < node.EndTimestamp)
+                {
+                    var duration = (long)Math.Floor((node.EndTimestamp - lastTimestamp).TotalMilliseconds);
+                    node.Timelines.Add(new TraceResponseTimeline(true, duration / (double)node.Duration));
+                }
+            }
+            else if (parent is not null)
+            {
+                Console.Out.WriteLine("internalParentLeft = {0}", internalParentLeft);
+                var marginLeft = internalParentLeft / (double)parent.Duration; // TODO:use top root duration!
+
+                var duration = (long)Math.Floor((node.EndTimestamp - node.Timestamp).TotalMilliseconds);
+                var durationPercent = (duration / (double)parent.Duration);
+                node.Timelines.Add(new TraceResponseTimeline(true, durationPercent, marginLeft));
+            }
             else
             {
-                if (_overView.SpansDeeps.ContainsKey(parentId))
-                    deep = _overView.SpansDeeps[parentId].Deep;
-
-                deep++;
-
-                add.Deep = deep;
-                _overView.SpansDeeps.Add(id, add);
-
-                if (_overView.SpanChildren.ContainsKey(parentId))
-                    _overView.SpanChildren[parentId].Add(id);
-                else
-                    _overView.SpanChildren.Add(parentId, new List<string> { id });
+                node.Timelines.Add(new TraceResponseTimeline(true, 1));
             }
         }
 
-        var sortIds = _overView.SpansDeeps.Values.OrderBy(item => item.Time).ThenBy(item => item.Deep).Select(item => item.Id).ToList();
-        _items = list.OrderBy(item => sortIds.IndexOf(item.SpanId));
+        return nodes;
     }
 
-    private void SetOverview()
+    private static string FormatDuration(long ms)
     {
-        _overView.Total = _items.Count();
-        _overView.SearchName = default!;
-        var data = _overView.SpansDeeps.Where(item => item.Value.IsTransaction && !_overView.SpansDeeps.ContainsKey(item.Value.ParentId)).ToList();
-        DateTime start = data.Min(item => item.Value.Time);
-        long total = data.Sum(item => item.Value.TimeUs);
-        _overView.Start = start;
-        _overView.TimeUs = total;
-        _overView.Name = _items.First().Name;
-        _overView.Services = _overView.SpansDeeps.Values.Select(item => item.ServiceName).Distinct().Select(item => new TraceOverviewServiceModel
+        if (ms < 1000)
         {
-            Name = item,
-            Color = CreateColor()
-        }).ToList();
+            return $"{ms}ms";
+        }
+
+        if (ms < 60000)
+        {
+            return $"{(ms / 1000d):F}s";
+        }
+
+        return $"{(ms / 60000d):F}m";
     }
 }
