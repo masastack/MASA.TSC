5df1c42adf2357ff3c9f6c3cca603a8b30119137
wuweilai <824255785@qq.com>
2022-12-15 18:20:57 +0800

    log

---
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Panel/Log/LogView.razor.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Web.Admin.Rcl.Components.Panel.Log;
+
+public partial class LogView
+{
+    [Parameter]
+    public Dictionary<string, LogTree>? JsonObject { get; set; }
+
+    [Parameter]
+    public List<LogTree>? JsonArray { get; set; }
+}
