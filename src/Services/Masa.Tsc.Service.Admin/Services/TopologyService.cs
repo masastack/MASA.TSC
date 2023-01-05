a4387ee9014b575f5cdc3bc2d9248048af67fafe
qinyouzeng <qinyouzeng@masastack.com>
2023-01-05 16:56:35 +0800

    chore: add test log

--- src/Services/Masa.Tsc.Service.Admin/Services/TopologyService.cs
+++ src/Services/Masa.Tsc.Service.Admin/Services/TopologyService.cs
@@ -5,13 +5,17 @@ namespace Masa.Tsc.Service.Admin.Services;
 
 public class TopologyService : ServiceBase
 {
-    public TopologyService() : base("/api/topology")
+    private readonly ILogger _logger;
+    public TopologyService(ILogger<TopologyService> logger) : base("/api/topology")
     {
         App.MapGet($"{BaseUri}/start", StartAsync);
+        _logger = logger;
     }
 
     public async Task StartAsync([FromServices] IEventBus eventBus, [FromQuery] DateTimeOffset? excuteTime)
     {
+        _logger.LogInformation($"TopologyService The number of processors on this computer is {Environment.ProcessorCount}");        
+
         if (excuteTime == null)
             excuteTime = new DateTimeOffset(DateTime.UtcNow);
         var end = excuteTime.Value.ToUniversalTime().DateTime;
