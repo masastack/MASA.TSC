dfb092455032d6ea04acd2cdcfbcd5aa7b7e1ff5
qinyouzeng <qinyouzeng@masastack.com>
2023-01-11 14:04:10 +0800

    chore: enable swagger

--- src/Services/Masa.Tsc.Service.Admin/Program.cs
+++ src/Services/Masa.Tsc.Service.Admin/Program.cs
@@ -118,11 +118,11 @@ var app = builder.Services
 app.UseMasaExceptionHandler();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseRouting();
 
 app.UseAuthentication();
