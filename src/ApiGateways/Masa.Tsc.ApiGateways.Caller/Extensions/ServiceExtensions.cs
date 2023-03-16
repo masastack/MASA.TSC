4923e16f048d8d68c44f72f25f64ab14c688f3b6
qinyouzeng <qinyouzeng@masastack.com>
2023-03-16 16:02:22 +0800

    chore: update using

--- src/ApiGateways/Masa.Tsc.ApiGateways.Caller/Extensions/ServiceExtensions.cs
+++ src/ApiGateways/Masa.Tsc.ApiGateways.Caller/Extensions/ServiceExtensions.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.StackSdks.Caller;
-
 namespace Masa.Tsc.ApiGateways.Caller;
 
 public static class ServiceExtensions
