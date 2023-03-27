e3d2e9bae1e99708fecade2c1862e371d260723e
wuweilai <824255785@qq.com>
2023-03-27 09:16:56 +0800

    refactor code

--- src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Dashboards/Configurations/PanelGridsExtensions.cs
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Dashboards/Configurations/PanelGridsExtensions.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using System.Linq;
-
 namespace Masa.Tsc.Web.Admin.Rcl.Components.Dashboards.Configurations;
 
 public static class PanelGridsExtensions
