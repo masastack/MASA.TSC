0d287e2c1f1270abdb86ea6e3281f8bd5eb21761
Li Ming <1028139084@qq.com>
2023-04-07 18:30:56 +0800

    delete comment

--- src/Web/Masa.Tsc.Web.Admin.Rcl/wwwroot/js/upload/meditor-upload-image.js
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/wwwroot/js/upload/meditor-upload-image.js
@@ -16,22 +16,7 @@ async function UploadImage(element, ossParamter) {
     const client = new OSS(ossParamter);
 
     const headers = {
-        // Ö¸¶¨¸ÃObject±»ÏÂÔØÊ±ÍøÒ³µÄ»º´æÐÐÎª¡£
-        // 'Cache-Control': 'no-cache',
-        // Ö¸¶¨¸ÃObject±»ÏÂÔØÊ±µÄÃû³Æ¡£
-        // 'Content-Disposition': 'oss_download.txt',
-        // Ö¸¶¨¸ÃObject±»ÏÂÔØÊ±µÄÄÚÈÝ±àÂë¸ñÊ½¡£
-        // 'Content-Encoding': 'UTF-8',
-        // Ö¸¶¨¹ýÆÚÊ±¼ä¡£
-        // 'Expires': 'Wed, 08 Jul 2022 16:57:01 GMT',
-        // Ö¸¶¨ObjectµÄ´æ´¢ÀàÐÍ¡£
-        // 'x-oss-storage-class': 'Standard',
-        // Ö¸¶¨ObjectµÄ·ÃÎÊÈ¨ÏÞ¡£
         'x-oss-object-acl': 'public-read-write',
-        // ÉèÖÃObjectµÄ±êÇ©£¬¿ÉÍ¬Ê±ÉèÖÃ¶à¸ö±êÇ©¡£
-        // 'x-oss-tagging': 'Tag1=1&Tag2=2',
-        // Ö¸¶¨CopyObject²Ù×÷Ê±ÊÇ·ñ¸²¸ÇÍ¬ÃûÄ¿±êObject¡£´Ë´¦ÉèÖÃÎªtrue£¬±íÊ¾½ûÖ¹¸²¸ÇÍ¬ÃûObject¡£
-        // 'x-oss-forbid-overwrite': 'true',
     };
 
     var result = [];
@@ -45,9 +30,6 @@ async function UploadImage(element, ossParamter) {
 
 async function putObject(client, file, headers) {
     try {
-        // ÌîÐ´ObjectÍêÕûÂ·¾¶¡£ObjectÍêÕûÂ·¾¶ÖÐ²»ÄÜ°üº¬BucketÃû³Æ¡£
-        // Äú¿ÉÒÔÍ¨¹ý×Ô¶¨ÒåÎÄ¼þÃû£¨ÀýÈçexampleobject.txt£©»òÎÄ¼þÍêÕûÂ·¾¶£¨ÀýÈçexampledir/exampleobject.txt£©µÄÐÎÊ½ÊµÏÖ½«Êý¾ÝÉÏ´«µ½µ±Ç°Bucket»òBucketÖÐµÄÖ¸¶¨Ä¿Â¼¡£
-        // data¶ÔÏó¿ÉÒÔ×Ô¶¨ÒåÎªfile¶ÔÏó¡¢BlobÊý¾Ý»òÕßOSS Buffer¡£
         const result = await client.put(
             `${file.name}`,
             file,
