916afa720429fabb45fce5f5c8f3f1b65b3c61bd
LiMing <38368335+MapleWithoutWords@users.noreply.github.com>
2023-04-10 11:26:22 +0800

    feat:add image upload to TextPanel component (#346)
    * feat:add image upload to TextPanel component
    
    * add file size limit
    
    * delete comment

---
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/wwwroot/js/upload/meditor-upload-image.js
@@ -0,0 +1,45 @@
+﻿async function EditorUploadImageToOss(editorContentElement, editorElement, ossParamter) {
+    var fileInputElement = editorElement.querySelector('input.ql-image[type=file]')
+    var imageFiles = await UploadImage(fileInputElement, ossParamter);
+
+    let quill = editorContentElement.__quill;//get quill editor
+    for (var i = 0; i < imageFiles.length; i++) {
+        let length = quill.getSelection().index;
+        quill.insertEmbed(length, 'image', imageFiles[i]);//Insert the uploaded picture into the editor
+        quill.setSelection(length + 1);
+    }
+}
+
+async function UploadImage(element, ossParamter) {
+    var imageFiles = element.files
+    console.log("UploadImage", ossParamter);
+    const client = new OSS(ossParamter);
+
+    const headers = {
+        'x-oss-object-acl': 'public-read-write',
+    };
+
+    var result = [];
+    for (var i = 0; i < imageFiles.length; i++) {
+        var url = await putObject(client, imageFiles[i], headers);
+        result.push(url);
+    }
+
+    return result
+}
+
+async function putObject(client, file, headers) {
+    try {
+        const result = await client.put(
+            `${file.name}`,
+            file,
+            {
+                headers
+            }
+        );
+        console.log(result);
+        return result.url;
+    } catch (e) {
+        console.log(e);
+    }
+}
