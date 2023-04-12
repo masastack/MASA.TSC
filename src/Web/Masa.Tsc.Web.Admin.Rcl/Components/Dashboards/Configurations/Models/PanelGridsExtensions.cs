7b59800446095d6973162ed815ba954175f4a20d
wuweilai <824255785@qq.com>
2023-04-12 17:07:05 +0800

    refactor code

--- src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Dashboards/Configurations/Models/PanelGridsExtensions.cs
+++ src/Web/Masa.Tsc.Web.Admin.Rcl/Components/Dashboards/Configurations/Models/PanelGridsExtensions.cs
@@ -14,10 +14,30 @@ public static class PanelGridsExtensions
                 panels.Add(panel);
                 return;
             }
+            var ys = panels.Select(panel => panel.Y).Distinct().ToList();
             for (int i = panels.Count - 1; i >= 0; i--)
             {
                 var itemPanel = panels[i];
-                if ((itemPanel.X + panel.Width + itemPanel.Width) > 12)
+                if (ys.Count > 1 && itemPanel.Y != 0)
+                {
+                    var onTopY = ys.Where(y => y < itemPanel.Y).Max();
+                    var onTopPanels = panels.Where(panel => panel.Y == onTopY);
+                    var rightPanel = onTopPanels.OrderByDescending(panel => panel.X).First();
+                    if (rightPanel.X + rightPanel.Width + panel.Width > 12)
+                    {
+                        if ((itemPanel.X + panel.Width + itemPanel.Width) > 12)
+                        {
+                            itemPanel.X = 0;
+                            itemPanel.Y += panel.Height;
+                        }
+                        else
+                        {
+                            itemPanel.X += panel.Width;
+                        }
+                    }
+                    else itemPanel.X += panel.Width;
+                }
+                else if ((itemPanel.X + panel.Width + itemPanel.Width) > 12)
                 {
                     itemPanel.X = 0;
                     itemPanel.Y += panel.Height;
