b835623060f419b6f515e6cee497016129092642
wzh425 <wuzihao425@outlook.com>
2023-05-18 11:58:12 +0800

    chore : Add migration

---
+++ src/Services/Masa.Tsc.Service.Admin/Migrations/20230518035731_IntegrationEventLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Masa.Tsc.Service.Admin.Migrations
+{
+    public partial class IntegrationEventLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExpandContent",
+                schema: "tsc",
+                table: "IntegrationEventLog",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpandContent",
+                schema: "tsc",
+                table: "IntegrationEventLog");
+        }
+    }
+}
