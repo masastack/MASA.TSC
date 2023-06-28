210cc648c49287f2547236b04c20b03a349dc66c
qinyouzeng <qinyouzeng@masastack.com>
2023-06-28 17:00:20 +0800

    add files

---
+++ src/Contracts/Masa.Tsc.Contracts.Admin/Logs/LogErrorDto.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Tsc.Contracts.Admin.Logs;
+
+public class LogErrorDto
+{
+    public string Message { get; set; }
+
+    public int Count { get; set; }
+}
