4ba983cccc03ab5b85bfb0ef16d578f03e9825bb
qinyouzeng <qinyouzeng@masastack.com>
2024-03-13 11:05:24 +0800

    chore: update

--- src/ApiGateways/Masa.Tsc.ApiGateways.Caller/Services/ApmService.cs
+++ src/ApiGateways/Masa.Tsc.ApiGateways.Caller/Services/ApmService.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.StackSdks.Tsc.Apm.Clickhouse.Models.Request;
-using Masa.Contrib.StackSdks.Tsc.Apm.Clickhouse.Models.Response;
-
 namespace Masa.Tsc.ApiGateways.Caller.Services;
 
 public sealed class ApmService : BaseService
