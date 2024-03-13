955d21b9c9312389abc9c0a8b99c322c183143aa
qinyouzeng <qinyouzeng@masastack.com>
2024-03-13 18:54:32 +0800

    fix: fix errors

--- src/Services/Masa.Tsc.Service.Admin/Services/ApmService.cs
+++ src/Services/Masa.Tsc.Service.Admin/Services/ApmService.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Tsc.Service.Admin.Extensions;
-
 namespace Masa.Tsc.Service.Admin.Services;
 
 public class ApmService : ServiceBase
